dde15953ab62bdf1e961b1af2c094041c7a03812
g535879 <guyubinn@qq.com>
2015-12-04 20:42:57 +0800

    wewq

---
+++ ViewControllers/SlideViewController.h
@@ -0,0 +1,13 @@
+//
+//  SlideViewController.h
+//  ShareMySoul
+//
+//  Created by 古玉彬 on 15/12/4.
+//  Copyright © 2015年 gf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlideViewController : UIViewController
+
+@end
---
+++ ViewControllers/SlideViewController.m
@@ -0,0 +1,37 @@
+//
+//  SlideViewController.m
+//  ShareMySoul
+//
+//  Created by 古玉彬 on 15/12/4.
+//  Copyright © 2015年 gf. All rights reserved.
+//
+
+#import "SlideViewController.h"
+
+@interface SlideViewController ()
+
+@end
+
+@implementation SlideViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
