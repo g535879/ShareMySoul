36c1f3f8f1e280270e2f0a40bda080c526cc4339
kenanwangyi <674904111@qq.com>
2015-12-04 08:57:45 +0800

    gaode key

--- ViewControllers/HomePageViewController.m
+++ ViewControllers/HomePageViewController.m
@@ -7,14 +7,10 @@
 //
 
 #import "HomePageViewController.h"
+@interface HomePageViewController (){
+    
 
-@interface HomePageViewController ()<MAMapViewDelegate,CLLocationManagerDelegate>{
-    //地图视图
-    MAMapView *_mapView;
-    //位置信息
-    CLLocationManager *_locationManager;
 }
-
 
 @end
 
@@ -23,57 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    //配置用户 Key
-    [MAMapServices sharedServices].apiKey = GEO_API_KEY;
-    
-    [self createMapView];
-}
-
-
-#pragma mark -创建地图视图
-- (void)createMapView{
-    
-    
-    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
-    _mapView.delegate = self;
-    _mapView.showsUserLocation = YES;
-    _mapView.userTrackingMode = MAUserTrackingModeFollow;
-    
-    [self.view addSubview:_mapView];
-    
-    
-    _locationManager = [[CLLocationManager alloc] init];
-    
-    [_locationManager requestWhenInUseAuthorization];
-    _locationManager.delegate = self;
-    //定位精度
-    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    //定位频率
-    _locationManager.distanceFilter = 100.0;
-    
-    //启动跟踪定位
-    [_locationManager startUpdatingLocation];
     
 }
 
-//#pragma mark -创建系统annotation
-//- (void)createSystemAnnotation{
-//    
-//    MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
-//    pointAnnotation.coordinate = CLLocationCoordinate2DMake(39.98, 116.48);
-//    
-//    [_mapView addAnnotation:pointAnnotation];
-//}
-
-#pragma mark -CLLocationManager代理协议
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
-
-    CLLocation *location = [locations firstObject];
-    CLLocationCoordinate2D coordinate = location.coordinate;
-    NSLog(@"经度：%f 纬度：%f 海拔：%f 航向：%f 行走速度：%f",coordinate.longitude,location.altitude,location.altitude,location.course,location.speed);
-    //[_locationManager stopUpdatingLocation];
-
-}
 
 
 
--- ViewControllers/NearbyViewController.h
+++ ViewControllers/NearbyViewController.h
@@ -10,4 +10,8 @@
 
 @interface NearbyViewController : BasicViewController
 
+
+
 @end
+
+
